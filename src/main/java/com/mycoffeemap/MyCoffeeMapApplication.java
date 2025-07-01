f922264a2f3aa329a9e3f804372ea36bca93f2ce
hyunju <mira2539@gmail.com>
2025-07-01 16:55:42 +0900

    엔티티추가

---
+++ src/main/java/com/mycoffeemap/MyCoffeeMapApplication.java
@@ -0,0 +1,13 @@
+package com.mycoffeemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoffeeMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoffeeMapApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mycoffeemap/user/User.java
@@ -0,0 +1,45 @@
+package com.mycoffeemap.user;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 200, nullable = false)
+	private String pass;
+	
+	@Column(length = 200, nullable = false)
+	private String nick;
+	
+	@Column(length = 200, nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private Date birth;
+	
+	@Column(unique = true ,length = 200, nullable = false)
+	private String email;
+	
+	@Column(length = 200)
+	private String profileImg;
+	
+	@Column(length = 200)
+	private String address;
+		
+}
